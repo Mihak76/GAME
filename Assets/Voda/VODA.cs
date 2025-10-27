55820560020cb8ed3d2298ff04df23351bae4184
Mihak76 <mihakocon1@gmail.com>
2025-10-27 16:16:47 +0100

    Voda

--- Assets/Voda/VODA.cs
+++ Assets/Voda/VODA.cs
@@ -4,18 +4,17 @@ public class TekočaVoda : MonoBehaviour
 {
     public float scrollSpeedX = 0.1f;
     public float scrollSpeedY = 0.0f;
-    private Material mat;
+    private Renderer rend;
 
     void Start()
     {
-        // Shranimo referenco, da se ne ustvarja kopija vsak frame
-        mat = GetComponent<Renderer>().material;
+        rend = GetComponent<Renderer>();
     }
 
     void Update()
     {
         float offsetX = Time.time * scrollSpeedX;
         float offsetY = Time.time * scrollSpeedY;
-        mat.mainTextureOffset = new Vector2(offsetX, offsetY);
+        rend.material.mainTextureOffset = new Vector2(offsetX, offsetY);
     }
 }
