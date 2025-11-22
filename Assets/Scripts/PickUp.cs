4cd8c07babada2518066bb7b07eada59db443c91
Mihak76 <mihakocon1@gmail.com>
2025-11-22 14:35:17 +0100

    ...

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,36 +1,33 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
 
 public class PickUp : MonoBehaviour
 {
-    public GameObject FlashLightOnPlayer;
-    public GameObject PickUpText;
+    public GameObject FlashLightOnPlayer; // svetilka, ki se aktivira
+    public GameObject PickUpText;         // tekst "Press E to pick up"
+
     void Start()
     {
-        FlashLightOnPlayer.SetActive(false);
-        PickUpText.SetActive(false);
-
+        FlashLightOnPlayer.SetActive(false); // na začetku izklopljeno
+        PickUpText.SetActive(false);         // tekst izklopljen
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
-            PickUpText.SetActive(true);
+            PickUpText.SetActive(true); // pokaže tekst, ko si ob svetilki
 
             if (Input.GetKey(KeyCode.E))
             {
-                this.gameObject.SetActive(false);
-
-                FlashLightOnPlayer.SetActive(true);
-                PickUpText.SetActive(false);
+                this.gameObject.SetActive(false);  // pobereš svetilko s tal
+                FlashLightOnPlayer.SetActive(true); // takoj se aktivira svetilka
+                PickUpText.SetActive(false);        // skrije tekst
             }
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        PickUpText.SetActive(false);
+        PickUpText.SetActive(false); // ko odideš, skrije tekst
     }
 }
