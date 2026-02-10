479938c33b36007e07ed65c7483109f3356ddc23
Mihak76 <mihakocon1@gmail.com>
2026-02-10 14:50:10 +0100

    hi

---
+++ Assets/Scripts/Ambience.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceSound : MonoBehaviour
+{
+    public Collider Area;            // The area of the sound
+    public GameObject Player;        // The object to track
+
+    void Update()
+    {
+        // Locate closest point on the collider to the player
+        Vector3 closestPoint = Area.ClosestPoint(Player.transform.position);
+        // Set position to closest point to the player
+        transform.position = closestPoint;
+    }
+}
