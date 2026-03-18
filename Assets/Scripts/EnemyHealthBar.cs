838e0fc64488011372b981ad85476d5a89466047
Mihak76 <mihakocon1@gmail.com>
2026-03-18 15:47:20 +0100

    sd

--- Assets/Scripts/EnemyHealthBar.cs
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class EnemyHealthBar : MonoBehaviour
 {
     public EnemyHealth enemyHealth;
-    public Slider slider;
     public TextMeshProUGUI healthText;
 
     int maxHealth;
@@ -13,12 +11,10 @@ public class EnemyHealthBar : MonoBehaviour
     void Start()
     {
         maxHealth = enemyHealth.health;
-        slider.maxValue = maxHealth;
     }
 
     void Update()
     {
-        slider.value = enemyHealth.health;
         healthText.text = enemyHealth.health + " / " + maxHealth;
     }
 }
